5a91506328c5ea1fdc8f25c26fc27cf6e4c999d0
Siddhartha Konduru <kondurus@usf.edu>
2024-09-15 17:14:51 -0400

    Create checkLowStock function

--- inventory_management.js
+++ inventory_management.js
@@ -4,23 +4,26 @@ const inventory = [
     { name: 'Television', price: 420, quantity: 53, lowStockLevel: 21 },  
     { name: 'Chair', price: 50, quantity: 79, lowStockLevel: 17 }, 
     { name: 'Table', price: 90, quantity: 56, lowStockLevel: 53 },  
-    { name: 'Bookshelf', price: 150, quantity: 38, lowStockLevel: 10 }    
+    { name: 'Bookshelf', price: 150, quantity: 38, lowStockLevel: 10 },
+    { name: 'Sofa', price: 120, quantity: 95, lowStockLevel: 37 },   
+    { name: 'Clock', price: 70, quantity: 37, lowStockLevel: 18 },  
+    { name: 'Desk', price: 40, quantity: 45, lowStockLevel: 25 }, 
+    { name: 'Computer', price: 50, quantity: 56, lowStockLevel: 7 },  
+    { name: 'Lamp', price: 70, quantity: 38, lowStockLevel: 11 }     
 ];
 
 //Create a Function to Display Product Details
 function displayProductDetails() {
     for (let i = 0; i < inventory.length; i++){
-        const {name, price, quantity, lowStockLevel} = inventory[i];
+        const {name, price, quantity, lowStockLevel} = inventory[i]; // For loop constant for inventory product details
         const stockStatus = quantity <= lowStockLevel ? "Low Stock" : "In Stock";
         // Logs details of each product
-        console.log(`Product Name: ${name}`);
-        console.log(`Price: ${price}`);
-        console.log(`Stock Status: ${stockStatus}`);
-        console.log(`Quantity: ${quantity}`);
-        console.log(`----- Product Details Displayed Above -----`);
+        console.log(`Product Name: ${name}, Product Price: $${price}`);
+        console.log(`Product Stock Status: ${stockStatus}, Product Quantity: ${quantity}`);
+        console.log(`********** Product Details Displayed Above **********`);
     }
 }
-displayProductDetails(); //Runs the function
+displayProductDetails(); //Runs function
 
 // Create a Function to Update Stock
 function updateStock(productName, unitsSold) {
@@ -28,8 +31,7 @@ function updateStock(productName, unitsSold) {
 
     for (const item of inventory) {
         if (item.name === productName) {
-            item.quantity -= unitsSold; // Update the quantity
-            productPresent = true;
+            item.quantity -= unitsSold; // Update quantity
 
             // Determine stock status based on updated quantity
             let stockStatus;
@@ -38,30 +40,42 @@ function updateStock(productName, unitsSold) {
             } else if (item.quantity < item.lowStockLevel) {
                 stockStatus = "Low Stock";
             } else {
-                stocktatus = "In Stock";
+                stockStatus = "In Stock";
             }
 
-            // Log tShe updated stock details
-            console.log(`Product Name: ${item.name}`);
-            console.log(`Updated Stock Status: ${stockStatus}`);
-            console.log(`Updated Quantity: ${item.quantity}`);;
+            // Log updated stock 
+            console.log(`Product Name: ${item.name}, Updated Stock Status: ${stockStatus}, and Updated Quantity: ${item.quantity}`);
     return;
 }
 }
 console.log(`${productName} not available`);
 
 }
-updateStock('Table', 8);
-updateStock('Watch', -80);
-updateStock('Bookshelf', 10);
+updateStock('Clock', 18);
+updateStock('Watch', -37);
+updateStock('Television', 3);
+updateStock('Lamp', -8);
+updateStock('Bookshelf', 21);
+
+// Create a Function to Check Low Stock Products
+function checkLowStock(inventory){
+    inventory.forEach(product => {
+        if(product.quantity < product.lowStockLevel){
+            console.log(product.name);
+        }
+    }
+    );
+}
+console.log("List of Products That Need Restocking");
+checkLowStock(inventory);
 
 //Create a Function to Calculate Total Inventory Value
 function calculateInventoryValue(inventory){
     let totalValue = 0;
     for (const item of inventory){
-        totalValue += item.quantity * item.price;
+        totalValue += item.quantity * item.price; // multiplies quantity by price for each product  
     }
     return totalValue;
 }
 const totalValue =calculateInventoryValue(inventory);
-console.log(`Total Inventory: $${totalValue}`);
+console.log(`Total Inventory Value: $${totalValue}`);
